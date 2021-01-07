1e716c91a44e01ee4e0e12f532d6b7da26fa5861
jriley-asm <jack@assembleinc.com>
2021-01-07 15:57:35 -0800

    first commit

---
+++ __mainifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Webhook App',
+    'description': 'Fire webhooks out of Odoo at things',
+    'author': 'Assemble Inc/Jack Riley',
+    'depends': ['base'],
+    'application': True
+}
