dadb451bb3dfdca11d3b2722b7a2a52d3fa33920
jriley-asm <jack@assembleinc.com>
2021-01-11 17:12:41 -0800

    changes to logger

--- shopify_connector/models/modified_sale_order.py
+++ shopify_connector/models/modified_sale_order.py
@@ -1,4 +1,7 @@
+import logging
 from odoo import fields, models, api
+
+_logger = logging.getLogger(__name__)
 
 class MyMixedInSaleOrder(models.Model):
     _name = 'sale.order'
@@ -6,6 +9,6 @@ class MyMixedInSaleOrder(models.Model):
 
     test_field = fields.Char("Jack's Field")
 
-    @api.depends('delivery_count')
+    @api.onchange('delivery_count')
     def _on_delivered_count_change(self):
        _logger.debug('THink the delivery count changed!')
