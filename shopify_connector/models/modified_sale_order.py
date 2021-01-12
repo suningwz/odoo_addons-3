9e249daec5c2a33c19af4645832407d157ec837f
jriley-asm <jack@assembleinc.com>
2021-01-12 09:21:46 -0800

    moved logic to stock move line

--- shopify_connector/models/modified_sale_order.py
+++ shopify_connector/models/modified_sale_order.py
@@ -3,15 +3,15 @@ from odoo import fields, models, api
 
 _logger = logging.getLogger(__name__)
 
-class MyMixedInSaleOrderLine(models.Model):
-    _name = 'sale.order.line'
-    _inherit = ['sale.order.line']
+class MyMixedInStockMoveLine(models.Model):
+    _name = 'stock.move.line'
+    _inherit = ['stock.move.line']
 
     #test_field = fields.Char("Jack's Field")
-    def create(self, values):
-        _logger.info('Called create')
-        record = super(MyMixedInSaleOrderLine, self).create(values)
-        return record
+    # def create(self, values):
+    #     _logger.info('Called create')
+    #     record = super(MyMixedInSaleOrderLine, self).create(values)
+    #     return record
 
     # def flush(fnames, records):
     #     _logger.info('Called flush')
@@ -24,24 +24,14 @@ class MyMixedInSaleOrderLine(models.Model):
     #     record = super(MyMixedInSaleOrderLine, self).update(values)
     #     return record
 
-    @api.depends('qty_delivered')
-    def on_delivered_count_change(self):
-        _logger.info('Called special function')
+    # @api.depends('qty_delivered')
+    # def on_delivered_count_change(self):
+    #     _logger.info('Called special function')
 
     def write(self, values):
        _logger.info('FYI: This is happening')
-       _logger.info('order id object type: ' + str(type(self['order_id'])))
-       order_id = self['order_id'].id
-       order_lines = self.env['sale.order.line'].search([('order_id', '=', order_id)])
-       total_quantity_to_deliver = 0
-       total_quantity_delivered = 0
-       for line in order_lines:
-           total_quantity_to_deliver += line['qty_to_deliver']
-           total_quantity_delivered += line['qty_delivered']
-       _logger.info('Total quantity delivered: ' + str(total_quantity_delivered))
-       _logger.info('Total quantity to deliver: ' + str(total_quantity_to_deliver))
-       if total_quantity_delivered == total_quantity_to_deliver:
+       if self['product_uom_qty'] == self['qty_done']:
            #then we can call our api and say that this order is delivered
            _logger.info('the quantities are equal !')
-       record = super(MyMixedInSaleOrderLine, self).write(values)
+       record = super(MyMixedInStockMoveLine, self).write(values)
        return record
