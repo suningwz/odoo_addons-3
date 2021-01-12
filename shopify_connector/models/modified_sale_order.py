2b20b7d22fd06fe156ce030294ec5a40c06b017c
jriley-asm <jack@assembleinc.com>
2021-01-12 08:47:32 -0800

    swapped update for flush function

--- shopify_connector/models/modified_sale_order.py
+++ shopify_connector/models/modified_sale_order.py
@@ -13,16 +13,16 @@ class MyMixedInSaleOrderLine(models.Model):
         record = super(MyMixedInSaleOrderLine, self).create(values)
         return record
 
-    # def flush(self, fnames, records):
-    #     _logger.info('Called flush')
-    #     _logger.info('Fnames: ' + str(fnames))
-    #     record = super(MyMixedInSaleOrderLine, self).flush(values)
-    #     return record
-
-    def update(self, values):
-        _logger.info('Called update on sales order line')
-        record = super(MyMixedInSaleOrderLine, self).update(values)
+    def flush(fnames, records):
+        _logger.info('Called flush')
+        _logger.info('Fnames: ' + str(fnames))
+        record = super(MyMixedInSaleOrderLine, self).flush()
         return record
+
+    # def update(self, values):
+    #     _logger.info('Called update on sales order line')
+    #     record = super(MyMixedInSaleOrderLine, self).update(values)
+    #     return record
 
     def write(self, values):
        _logger.info('FYI: This is happening')
