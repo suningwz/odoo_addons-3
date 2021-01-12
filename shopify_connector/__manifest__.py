0fcc8ce11da007cf10e3c78073bd0a0f0f913cbd
jriley-asm <jack@assembleinc.com>
2021-01-12 11:52:11 -0800

    added group to view

--- shopify_connector/__manifest__.py
+++ shopify_connector/__manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        #'views/connector_menu.xml',
+        'views/connector_menu.xml',
         'security/shopify_connector_security.xml',
         'security/ir.model.access.csv'
     ],
