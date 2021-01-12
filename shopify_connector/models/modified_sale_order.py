cc96322e2d3a225dcd9ff0c5c78660526b984be9
jriley-asm <jack@assembleinc.com>
2021-01-11 16:38:22 -0800

    added test field

--- shopify_connector/models/modified_sale_order.py
+++ shopify_connector/models/modified_sale_order.py
@@ -4,6 +4,8 @@ class MyMixedInSaleOrder(models.Model):
     _name = 'sale.order'
     _inherit = ['sale.order']
 
+    test_field = fields.Char("Jack's Field")
+    
     @api.onchange('delivery_count')
     def _on_delivered_count_change(self):
        _logger.debug('THink the delivery count changed!')
