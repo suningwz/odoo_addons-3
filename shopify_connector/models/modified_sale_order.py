cd8903a4915ed4bef099c092b5400af54d62ec67
jriley-asm <jack@assembleinc.com>
2021-01-11 16:57:50 -0800

    including the file

--- shopify_connector/models/modified_sale_order.py
+++ shopify_connector/models/modified_sale_order.py
@@ -5,7 +5,7 @@ class MyMixedInSaleOrder(models.Model):
     _inherit = ['sale.order']
 
     test_field = fields.Char("Jack's Field")
-    
-    @api.onchange('delivery_count')
+
+    @api.depends('delivery_count')
     def _on_delivered_count_change(self):
        _logger.debug('THink the delivery count changed!')
