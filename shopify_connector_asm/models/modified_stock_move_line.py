8d8e837bea83f9af235ab08449ff8a51c18bb985
jriley-asm <jack@assembleinc.com>
2021-01-15 07:46:38 -0800

    change to write function in modified stock move line

--- shopify_connector_asm/models/modified_stock_move_line.py
+++ shopify_connector_asm/models/modified_stock_move_line.py
@@ -7,19 +7,20 @@ class MyMixedInStockMoveLine(models.Model):
     _name = 'stock.move.line'
     _inherit = ['stock.move.line']
 
-    @api.one
+
     def write(self, values):
        _logger.info('FYI: This is happening')
-       if self['product_uom_qty'] == self['qty_done']:
-           #then we can call our api and say that this order is delivered
-           _logger.info('the quantities are equal !')
-           #now we want to get the sale.order id of this stock move
-           stock_move_parent = self.env['stock.move'].search([['move_id', '=', move_id]])
-           _logger.info('stock move parent: ' + stock_move_parent)
-           #once we have that id, we can get the order and its associated store Domain, and shopify order domain
+       for rec in self:
+           if rec['product_uom_qty'] == rec['qty_done']:
+               #then we can call our api and say that this order is delivered
+               _logger.info('the quantities are equal !')
+               #now we want to get the sale.order id of this stock move
+               stock_move_parent = self.env['stock.move'].search([['move_id', '=', rec['move_id']]])
+               _logger.info('stock move parent: ' + stock_move_parent)
+               #once we have that id, we can get the order and its associated store Domain, and shopify order domain
 
-           #once we have the store domain we can hit up shopify's order api with those keys
+               #once we have the store domain we can hit up shopify's order api with those keys
 
-           #and update the order status to delivered.
-       record = super(MyMixedInStockMoveLine, self).write(values)
-       return record
+               #and update the order status to delivered.
+           record = super(MyMixedInStockMoveLine, self).write(values)
+           return record
