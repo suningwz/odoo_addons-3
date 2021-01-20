5a864689cf41f232cda08c47c56049cd59ed65fa
jriley-asm <jack@assemble.com>
2021-01-19 17:00:08 -0800

    added logic to custom sales orders for on order cancelled

--- shopify_connector_asm/models/modified_sales_order.py
+++ shopify_connector_asm/models/modified_sales_order.py
@@ -9,3 +9,32 @@ class MyMixedInSaleOrder(models.Model):
 
     platform_order_id = fields.Char('Platform Order ID')
     shopify_store_domain = fields.Char('Shopify Store Domain')
+
+    def write(self, values):
+       _logger.info('FYI: This is happening')
+       for rec in self:
+           if rec['shopify_store_domain'] != '':
+               print('thinks we should let know shopify know...maybe')
+               shopify_store_domain = rec['shopify_store_domain']
+               shopify_order_id = rec['platform_order_id']
+               ### CANCELLED CASE ###
+               if rec['state'] == 'cancel':
+                   shopify_store = self.env['shopify_connector.store'].search([['shop_name', '=', shopify_store_domain]])
+                   api_key = shopify_store['api_key']
+                   password = shopify_store['password']
+                   order_shop_url = "https://" + str(api_key) + ":" + str(password) +"@" + str(shopify_store_domain) + ".myshopify.com/admin/api/2020-10/orders/" + str(shopify_order_id) + "/cancel.json"
+
+                   headers = {
+                    'X-Shopify-Access-Token': password,
+                    'Content-Type': "application/json"
+                   }
+
+                   #no refund
+                   #get reckt
+                   cancel_json = {
+
+                   }
+
+                   response = requests.post(order_shop_url, json=cancel_json, headers=headers)
+           record = super(MyMixedInStockMoveLine, self).write(values)
+           return record
--- shopify_connector_asm/models/modified_stock_move_line.py
+++ shopify_connector_asm/models/modified_stock_move_line.py
@@ -20,37 +20,42 @@ class MyMixedInStockMoveLine(models.Model):
                sales_order_line = self.env['sale.order.line'].search([['id', '=', stock_move_parent['sale_line_id'].id]])
                sales_order = self.env['sale.order'].search([['id', '=', sales_order_line['order_id'].id]])
                #once we have that id, we can get the order and its associated store Domain, and shopify order id
-               shopify_store_domain = sales_order['shopify_store_domain']
-               shopify_order_id = sales_order['platform_order_id']
-               #now we want to look up our shopify stores against this domain
-               shopify_store = self.env['shopify_connector.store'].search([['shop_name', '=', shopify_store_domain]])
-               api_key = shopify_store['api_key']
-               password = shopify_store['password']
-               fulfillment_shop_url = "https://" + str(api_key) + ":" + str(password) +"@" + str(shopify_store_domain) + ".myshopify.com/admin/api/2020-10/orders/" + str(shopify_order_id) + "/fulfillments.json"
+               #its actually possible that they got an order from a platform that isnt shopify,
+               #in which case we've broken all their write operations for this record
+               #lets add an if to ask if the sales order contains the shopify store Domain
+               if sales_order['shopify_store_domain'] != '':
+                   shopify_store_domain = sales_order['shopify_store_domain']
+                   shopify_order_id = sales_order['platform_order_id']
+                   #now we want to look up our shopify stores against this domain
+                   shopify_store = self.env['shopify_connector.store'].search([['shop_name', '=', shopify_store_domain]])
+                   api_key = shopify_store['api_key']
+                   password = shopify_store['password']
+                   fulfillment_shop_url = "https://" + str(api_key) + ":" + str(password) +"@" + str(shopify_store_domain) + ".myshopify.com/admin/api/2020-10/orders/" + str(shopify_order_id) + "/fulfillments.json"
 
-               #fulfillment locaion id hardcoded here
-               #for now we're going to change this to the first location the store offers
-               #there should always be at least one
-               #it bears mentioning this location could totally be wrong
-               locations_shop_url = "https://" + str(api_key) + ":" + str(password) +"@" + str(shopify_store_domain) + ".myshopify.com/admin/api/2020-10/locations.json"
+                   #fulfillment locaion id hardcoded here
+                   #for now we're going to change this to the first location the store offers
+                   #there should always be at least one
+                   #it bears mentioning this location could totally be wrong
+                   locations_shop_url = "https://" + str(api_key) + ":" + str(password) +"@" + str(shopify_store_domain) + ".myshopify.com/admin/api/2020-10/locations.json"
 
-               headers = {
-                'X-Shopify-Access-Token': password,
-                'Content-Type': "application/json"
-               }
+                   headers = {
+                    'X-Shopify-Access-Token': password,
+                    'Content-Type': "application/json"
+                   }
 
-               locations_response = requests.get(locations_shop_url, headers=headers)
+                   locations_response = requests.get(locations_shop_url, headers=headers)
 
-               first_location_id = locations_response.json()['locations'][0]['id']
+                   first_location_id = locations_response.json()['locations'][0]['id']
 
-               fulfillment_json = {
-                  "fulfillment": {
-                    "location_id": first_location_id,
-                    "shipment_status": "delivered",
-                    "notify_customer": True
-                  }
-                }
-               response = requests.post(fulfillment_shop_url, json=fulfillment_json, headers=headers)
-               _logger.info(response.json())
+                   fulfillment_json = {
+                      "fulfillment": {
+                        #### THIS IS A HUGE PROBLEM ####
+                        "location_id": first_location_id,
+                        "shipment_status": "delivered",
+                        "notify_customer": True
+                      }
+                    }
+                   response = requests.post(fulfillment_shop_url, json=fulfillment_json, headers=headers)
+                   _logger.info(response.json())
            record = super(MyMixedInStockMoveLine, self).write(values)
            return record
